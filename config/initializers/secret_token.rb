33e3b197cf3e88313facb4223a04296941a74057
JayMcDowell <jay@musicianshalloffame.com>
2013-05-29 11:53:46 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSBlog::Application.config.secret_token = 'e7b773af95176d312b0a105eb26e6ba8801f0bf2fe9141e731dd490b24f9e2664fdac4595d7e7a73ba2cae32b4157158b0d1ef5fbd2599d8682396562111d096'
