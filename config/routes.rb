f8935aa14a567c08a45f4907f30b6208036853fb
JayMcDowell <jay@musicianshalloffame.com>
2013-06-04 09:44:46 -0500

    End of day 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NSSBlog::Application.routes.draw do
+  resources :posts, only: :index
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -1,15 +1,12 @@
 require 'test_helper'
 
 class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
-
   test "browse articles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
     Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writer :( :( lolol")
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
-    #assume we have a page variable
-    assert_include page.content, "First Post!!"
-    assert_include page.content, "Wow, it's been a while!"
+    visit '/posts'
+    assert_include page.body, "First Post!!"
+    #=>assert page.content.include? "First Post!!"
+    assert_include page.body, "Wow, it's been a while!"
   end
 end
